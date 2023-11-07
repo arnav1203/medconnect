59ab46e8153b2b776dd5e0485e469f40d7dc5b9e
arnav1203 <2101641530040@psit.ac.in>
2023-11-07 12:19:28 +0530

    Patvscon buttons

--- src/Components/Patvscon.js
+++ src/Components/Patvscon.js
@@ -81,6 +81,12 @@ export default function Patvscon() {
                                 </div>
                             </div>
                         </div>
+                        <div className='w-full p-10'>
+                            <a href="#_" class="relative inline-flex items-center justify-start px-6 py-3 overflow-hidden font-medium transition-all bg-white rounded hover:bg-white group">
+                                <span class="w-48 h-48 rounded rotate-[-40deg] bg-orange-600 absolute bottom-0 left-0 -translate-x-full ease-out duration-500 transition-all translate-y-full mb-9 ml-9 group-hover:ml-0 group-hover:mb-32 group-hover:translate-x-0"></span>
+                                <span class="relative w-full text-left text-black transition-colors duration-300 ease-in-out group-hover:text-white">See All Patient Resources</span>
+                            </a>
+                        </div>
                     </div>
                 </div>
                 <div className='w-2/5'>
@@ -155,6 +161,12 @@ export default function Patvscon() {
                                     </a>
                                 </div>
                             </div>
+                        </div>
+                        <div className='w-full p-10'>
+                            <a href="#_" class="relative inline-flex items-center justify-start px-6 py-3 overflow-hidden font-medium transition-all bg-white rounded hover:bg-white group">
+                                <span class="w-48 h-48 rounded rotate-[-40deg] bg-orange-600 absolute bottom-0 left-0 -translate-x-full ease-out duration-500 transition-all translate-y-full mb-9 ml-9 group-hover:ml-0 group-hover:mb-32 group-hover:translate-x-0"></span>
+                                <span class="relative w-full text-left text-black transition-colors duration-300 ease-in-out group-hover:text-white">See All Contributor's Resources</span>
+                            </a>
                         </div>
                     </div>
                 </div>
