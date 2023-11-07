f91d8a1558d10958e17a1859600817491fb0217f
arnav1203 <2101641530040@psit.ac.in>
2023-11-08 00:21:46 +0530

    Login page frontend

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -7,20 +7,21 @@ export default function Login() {
                 <div className='text-4xl flex justify-center w-full m-2'>Welcome Back!</div>
                 <div className='flex justify-center text-xl text-gray-600 m-2'>Login to access your account</div>
                 <label className='flex flex-col w-full p-1' >
-                    Email
-                    <input type='text' className='w-full rounded-md h-12 bg-slate-300 border border-gray-600 placeholder:text-gray-600 p-2' placeholder='Enter Email' />
+                    <p className='text-xl flex justify-start text-white'>Email:</p>
+                    <input type='text' className='w-full rounded-md h-12 border border-gray-600 placeholder:text-gray-600 p-2' placeholder='Enter Email' />
                 </label>
                 <label className='flex flex-col w-full p-1'>
-                    Password
-                    <input type='password' className='w-full rounded-md h-12 bg-slate-300 border border-gray-600 placeholder:text-gray-600 p-2' placeholder='Enter password' />
+                    <p className='text-xl flex justify-start text-white'>Password:</p>
+                    <input type='password' className='w-full rounded-md h-12 border border-gray-600 placeholder:text-gray-600 p-2' placeholder='Enter password' />
                 </label>
                 <div className='flex justify-center'>
-                    <button className="relative mt-5 inline-flex items-center border border-cyan-500 justify-start px-6 py-2 text-xl overflow-hidden font-medium transition-all bg-white rounded hover:bg-white group">
-                        <span className="w-48 h-48 rounded rotate-[-40deg] bg-cyan-600 absolute bottom-0 left-0 -translate-x-full ease-out duration-500 transition-all translate-y-full mb-9 ml-16 group-hover:ml-8 group-hover:mb-32 group-hover:translate-x-0"></span>
-                        <span className="relative w-full text-left text-black transition-colors duration-300 ease-in-out group-hover:text-white">Login</span>
+                    <button className="relative mt-5 flex justify-center items-center border border-cyan-500 w-48 py-2 text-xl overflow-hidden font-medium transition-all bg-white rounded hover:bg-white group">
+                        <span className="w-48 h-64 rounded rotate-[-40deg] bg-cyan-600 absolute bottom-0 left-0 -translate-x-full ease-out duration-500 transition-all translate-y-full mb-9 ml-16 group-hover:ml-8 group-hover:mb-32 group-hover:translate-x-0"></span>
+                        <div><span className="relative w-full text-left text-black transition-colors duration-300 ease-in-out group-hover:text-white">Login</span>
+                        </div>
                     </button>
                 </div>
-                <div className='flex justify-center p-3'><span>Not a member?</span><a href='/Signup' className='text-blue-700'>Signup Here</a></div>
+                <div className='flex justify-center p-3'><span>Not a member?</span><a href='/Signup' className='text-indigo-800 hover:underline'>Signup Here</a></div>
 
             </form>
         </div>
