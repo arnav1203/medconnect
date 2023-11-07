1544e07aa5694237e31ad6cadf7244054a2a364d
arnav1203 <2101641530040@psit.ac.in>
2023-11-07 17:35:58 +0530

    Buttons border

--- src/Components/Patvscon.js
+++ src/Components/Patvscon.js
@@ -82,8 +82,8 @@ export default function Patvscon() {
                             </div>
                         </div>
                         <div className='w-full p-10'>
-                            <a href="#_" class="relative inline-flex items-center justify-start px-6 py-3 overflow-hidden font-medium transition-all bg-white rounded hover:bg-white group">
-                                <span class="w-48 h-48 rounded rotate-[-40deg] bg-orange-600 absolute bottom-0 left-0 -translate-x-full ease-out duration-500 transition-all translate-y-full mb-9 ml-9 group-hover:ml-0 group-hover:mb-32 group-hover:translate-x-0"></span>
+                            <a href="#_" class="relative inline-flex border border-orange-500 items-center justify-start px-6 py-3 overflow-hidden font-medium transition-all bg-white rounded hover:bg-white group">
+                                <span class="w-60 h-52 rounded rotate-[-40deg] bg-orange-600 absolute bottom-0 left-0 -translate-x-full ease-out duration-500 transition-all translate-y-full mb-9 ml-9 group-hover:ml-0 group-hover:mb-32 group-hover:translate-x-0"></span>
                                 <span class="relative w-full text-left text-black transition-colors duration-300 ease-in-out group-hover:text-white">See All Patient Resources</span>
                             </a>
                         </div>
@@ -163,8 +163,8 @@ export default function Patvscon() {
                             </div>
                         </div>
                         <div className='w-full p-10'>
-                            <a href="#_" class="relative inline-flex items-center justify-start px-6 py-3 overflow-hidden font-medium transition-all bg-white rounded hover:bg-white group">
-                                <span class="w-48 h-48 rounded rotate-[-40deg] bg-orange-600 absolute bottom-0 left-0 -translate-x-full ease-out duration-500 transition-all translate-y-full mb-9 ml-9 group-hover:ml-0 group-hover:mb-32 group-hover:translate-x-0"></span>
+                            <a href="#_" class="relative inline-flex items-center border border-orange-500 justify-start px-6 py-3 overflow-hidden font-medium transition-all bg-white rounded hover:bg-white group">
+                                <span class="w-60 h-64 rounded rotate-[-40deg] bg-orange-600 absolute bottom-0 left-0 -translate-x-full ease-out duration-500 transition-all translate-y-full mb-9 ml-16 group-hover:ml-8 group-hover:mb-32 group-hover:translate-x-0"></span>
                                 <span class="relative w-full text-left text-black transition-colors duration-300 ease-in-out group-hover:text-white">See All Contributor's Resources</span>
                             </a>
                         </div>
